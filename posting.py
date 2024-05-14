c75f2eba3a51d4e81a7f22afa383041316db2965
mirelleg <99463358+mirelleg@users.noreply.github.com>
2024-05-13 19:24:08 -0700

    created posting.py

---
+++ posting.py
@@ -0,0 +1,17 @@
+class Posting:
+    # The document name/id the token was found in.
+    # Its tf-idf score for that document (for MS1, add only the term frequency
+    docName = ""
+    docID = -1
+    score = -1
+
+    def __init__(self, docID, score):
+        self.docID = docID
+        self.score = score
+    
+    def getDocID(self):
+        return self.docID
+
+    def getScore(self):
+
+        return self.score
