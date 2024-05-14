14d1d3c8e77e64ca50d8d1e63fcbe3315507cdb2
mirelleg <99463358+mirelleg@users.noreply.github.com>
2024-05-13 19:27:42 -0700

    deleted comments to start the fix of index merger

--- indexer.py
+++ indexer.py
@@ -64,7 +64,7 @@ def buildIndex():
     threshold = 0
     tokens_counter = 0
     #for d in docs:
-    for dirpath, dirnames, filenames in os.walk('DEV'):
+    for dirpath, dirnames, filenames in os.walk('ANALYST'):
         for file in filenames:
             print(f'file{id}')
             id += 1
@@ -77,62 +77,14 @@ def buildIndex():
                 tokens_dict = computeWordFrequencies(tokens)
 
                 for t in tokens_dict.keys():
-                    # print(f'processed token{tokens_counter}')
-                    #x = Posting(id, tokens_dict[t])
-                    #if threshold < 1000:
-                    #    with open(f"file{file_num}", 'a') as file:
-                    #        file.write(f"{t}: {str(x)},")
-                        #    threshold += 1
                     if t not in index_hash:
                         index_hash[t] = [Posting(id, tokens_dict[t])]
-
-                        #index_hash[t] = f'{id},{tokens_dict[t]}'
-                    # if t not in final_hash:
-                    #     final_hash[t] = ''
                     
                     else:
                         index_hash[t].append(Posting(id, tokens_dict[t]))
-                        #threshold = 0
-                        #file_num += 1
-                        #with open(f"file{file_num}", 'a') as file:
-                        #    file.write(f"{t}: {str(x)},")
-                        #    threshold += 1
-                        # index_hash[t] = index_hash[t]+f' {id},{tokens_dict[t]}'
-
-
+                    
                     tokens_counter += 1
-                    # if tokens_counter == 40000:
-                    #     file_name = f"json_dumps/dump{file_num}"
-                    #     with open(file_name, 'w') as out_file:
-                    #         json.dump(index_hash, out_file)
-                    #     tokens_counter = 0
-                    #     file_num += 1
-                    #     index_hash = {}
-                            
-
-
-                    # if t not in index_hash.keys():
-                    #     posting_list = []
-                    #     posting_list.append(Posting(id,tokens_dict[t]))
-                    #     index_hash[t] = posting_list
-                    # else:
-                    #     index_hash[t].append(Posting(id,tokens_dict[t]))
-    # line_num = 0
-    # with open('master', 'w') as out:
-    #     for key in final_hash.keys():
-    #         final_hash[key] = (line_num)
-    #         line_num += 1
-    #         for dirpath, dirnames, filenames in os.walk('json_dumps'):
-    #             for file in filenames:
-    #                 filepath = os.path.join(dirpath, file)
-    #                 with open(filepath, 'r') as f:
-    #                     loaded_dict = json.load(f)
-    #                     if key in loaded_dict.keys():
-    #                         out.write(f'{loaded_dict[key]} ')
-        
-    # with open("memory_dict", "w") as memory:
-    #     json.dump(final_hash, memory)
-
+                    
                     
 
 
