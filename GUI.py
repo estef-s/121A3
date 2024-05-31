66efb799593ee5965812757b9d87b14ba93060d7
Lanni <63504496+lanidv2433@users.noreply.github.com>
2024-05-30 22:54:24 -0700

    updated GUI

--- GUI.py
+++ GUI.py
@@ -1,5 +1,8 @@
 import streamlit as sl
+import searcher as s
+import time 
 
+s.setUP()
 sl.write('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">', unsafe_allow_html=True)
 
 def icon(icon_name):
@@ -8,16 +11,26 @@ def icon(icon_name):
 background = """
 <style>
     .stApp {
-        background-color: #4E5166;
+        background-color: #F3C2CC;
     }
     .stApp h1{
-        color: #B9B7A7;
+        color: #4E5166;
     }
     .stApp h3{
-        color: #B9B7A7;
+        color: #4E5166;
     }
     .stApp p{
-        color: #B9B7A7;
+        color: #4E5166;
+    }
+
+    a.custom-link {
+    color: #B9B7A7; /* Tomato color */
+    text-decoration: none;
+    }
+
+    a.custom-link:hover {
+        color: #ff4500; /* OrangeRed color */
+        text-decoration: underline;
     }
 </style>
 """
@@ -25,26 +38,29 @@ background = """
 sl.markdown(background, unsafe_allow_html=True)
 
 
-sl.title("Searching for...")
+sl.title("What do you want to search for?")
 
 # icon_pic = sl.markdown(icon("search") + " Search", unsafe_allow_html=True)
 # query = sl.text_input("Enter your search", value="")
 
 
 icon_pic = sl.markdown(icon("search") + " Search", unsafe_allow_html=True)
-query = sl.text_input(label = "", value = "")
+query = sl.text_input(label = "Enter Search", value = "")
 
-
-
-# # Display a text input box in the second column
-# with col2:
-#     user_input = st.text_input("Search", value="", help="Search for something")
-
-# # Add a search icon to the first column
-# with col1:
-#     st.markdown(icon("search"), unsafe_allow_html=True)
 
 sl.subheader("Previous Search") #h3
 
 if query:
-    sl.write(query)
+    # results = s.startEngine(query)
+    # sl.write(query)
+    start_time = time.time()  # Start time measurement
+    results = s.startEngine(query)
+    end_time = time.time()  # End time measurement
+    search_duration = end_time - start_time  # Calculate search duration
+
+    sl.write(f"Search completed in {search_duration:.2f} seconds")  # Display search time
+    sl.write(f"Results for \"{query}\"")
+    i = 1
+    for r in results:
+        sl.write(f"{i}) {r}")
+        i += 1
