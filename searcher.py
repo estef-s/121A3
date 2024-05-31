702ef3fd6a8c0943bb2472abc04268751c85b7da
Lanni <63504496+lanidv2433@users.noreply.github.com>
2024-05-30 22:55:25 -0700

    fixed cosine

--- searcher.py
+++ searcher.py
@@ -1,15 +1,17 @@
 import json
 import indexer
 from nltk.stem.porter import PorterStemmer
-import time
+import time 
+
+
 
 memoryIndex = {}
 docNames = {}
-doc_lengths = {}
-def startEngine():
-    #load index of index into memoryIndex
+
+def setUP():
     global memoryIndex
     global docNames
+    #load index of index into memoryIndex
     x = open('indexIndex.txt', 'r')
     y = open('docUrl.txt', 'r')
     memoryIndex = json.load(x)
@@ -17,58 +19,48 @@ def startEngine():
     #load doc names into docNames
     docNames = json.load(y)
 
-   # print("THIS IS MEMORY INDEX\n", memoryIndex)
+def startEngine(query):
+    #take in input
+    
+    #query = input("What do you want to search for?\n")
+    #split up input
+    stemmer = PorterStemmer()
+    time.time()
+    tokens = [stemmer.stem(t) for t in query.split()]
+    #tokens = query.split()
+    #print("THIS IS TOKENS\n", tokens)
+    #print(tokens)
+    
+    tokenDict = {}
+    for token in tokens:
+        #get the posting lists from in masterindex.txt using seek
+        x = json.loads(findTokenList(token))
+        docIds = []
+        for i in x:
+            docIds.append(i.get('docID'))
+        tokenDict[token] = set(docIds)
 
-    while True:
-        #take in input
-        query = input("What do you want to search for?\n")
         
-        #split up input
-        stemmer = PorterStemmer()
-        tokens = [stemmer.stem(t) for t in query.split()]
-       
-        #tokens = query.split()
-        #print("THIS IS TOKENS\n", tokens)
-        #print(tokens)
-        
-        tokenDict = {}
-        for token in tokens:
-            #get the posting lists from in masterindex.txt using seek
-            y = findTokenList(token)
-            x = json.loads(y)
-            docIds = []
-            for i in x:
-                docIds.append(i.get('docID'))
-            tokenDict[token] = set(docIds)
+    #     print("THIS is X\n", docIds)
+    #     print("THIS IS SET X\n", set(docIds))
 
-            
-        #     print("THIS is X\n", docIds)
-        #     print("THIS IS SET X\n", set(docIds))
-
-        # print("TOKEN DICTIONARY", tokenDict)
-        set_list = []
-        for v in tokenDict.values():
-            set_list.append(v)
-       # print("THIS IS V\n", v)
-        
-        intersect_docID = list(set.intersection(*set_list))
-
-        sorted_top_links = getdocURLS(cosineScore(tokens))
-        #print("THIS IS INTERSECT DOCID", intersect_docID)
-        #return intersection of list
-        
-        #go through docnames and match up doc ids w/ url
-        #print("THIS IS docNAMES\n", docNames)
-        urls = getdocURLS(intersect_docID)
-        print(f"Here are the top 5 links for {query}:")
-
-        test_top_links = []
-        for i in range(len(urls)):
-            if sorted_top_links[i] in urls:
-                test_top_links.append(sorted_top_links[i])
-
-        print(test_top_links[:5], "\n")
-        #break
+    # print("TOKEN DICTIONARY", tokenDict)
+    set_list = []
+    for v in tokenDict.values():
+        set_list.append(v)
+    # print("THIS IS V\n", v)
+    
+    intersect_docID = list(set.intersection(*set_list))
+    #print("THIS IS INTERSECT DOCID", intersect_docID)
+    #return intersection of list
+    
+    #go through docnames and match up doc ids w/ url
+    #print("THIS IS docNAMES\n", docNames)
+    urls = getdocURLS(intersect_docID)
+    print(f"Here are the top 5 links for {query}:")
+    print(urls[:5], "\n")
+    #print(f"Search time: {(time.time()-start_time)*1000} ms\n")
+    return urls
 
 
 def findTokenList(token):
@@ -77,6 +69,7 @@ def findTokenList(token):
         position = memoryIndex[token]
         file.seek(position)
         list_d = json.loads(file.readline()) #change to json.loads
+        print(type(list_d))
     #print("THIS IS LIST\n", list[token])
     return list_d[token]
 
@@ -86,68 +79,6 @@ def getdocURLS(docList):
     for doc in docList:
         urlList.append(docNames[str(doc)])
     return urlList
-
-def computeWordFrequencies(tokenList):
-    wordFreq = {}
-    for token in tokenList:
-        if token in wordFreq: # if already in dictionary, add to frequency otherwise set to 1
-            wordFreq[token] += 1
-        else: 
-            wordFreq[token] = 1
-    return wordFreq
-
-def cosineScore(query):
-    global doc_lengths
-    scores = {}
-    length_ = [] 
-    # stemmer = PorterStemmer()
-    # tokens = [stemmer.stem(t) for t in query.split()]
-    for t in query:
-        posting_list = findTokenList(t)
-        wordFreq = computeWordFrequencies(query)
-       # print("THIS IS WORD FREQ", wordFreq)
-        w_tq = wordFreq[t]
-        #print(w_tq)
-        #time.sleep(20)
-       # print("THIS iS POSTING LIST", posting_list)
-       # print("THIS IS THE TYPE FOR POSTING LIST", type(posting_list))
-        post_l = json.loads(posting_list)
-        for pl in post_l:
-            print("this is PL", pl)
-            print(type(pl))
-            # print(pl)
-            if pl['docID'] not in scores:
-                scores[pl['docID']] = pl['score'] * w_tq
-            else: 
-                scores[pl['docID']] += pl['score'] * w_tq
-            print("PL SCORES||||||||||||||", pl['score'])
-
-    
-    #array_length = length_(scores.keys())
-    z = open('docLengths.txt', 'r')
-    doc_lengths = json.load(z)
-    print(doc_lengths)
-    for k in scores.keys():
-        scores[k] = scores[k] / doc_lengths[str(k)] # FIX LENGTH NUMBER (REPLACE 500)
-
-    sorted_scores = sorted(scores, key = lambda x: scores[x], reverse = True)
-
-    return sorted_scores
-   
-
-    
-    # print("THIS IS sorted scores", sorted_scores)
-    # print("THIS IS SCORES", scores)
-
-
-    #print("THIS IS SCORES", scores)
-    
-
-    #return top k components         
-
-
-
-
 
 if __name__ == '__main__':
     #indexer.buildIndex()
